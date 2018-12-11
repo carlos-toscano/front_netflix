a361d8a29d8c17fd3f49016aadcfb36ffdc9b0a2
Carlos Toscano <lic.carlostoscano@gmail.com>
2018-12-10 21:41:36 -0700

    Se agrega final

---
+++ src/components/Movies/test/Movies.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Movies from './../Movies';
+
+describe('<Movies/>', () => {
+  it('Debe ejecutar render correctamente', () => {
+    const component = shallow(<Movies/>);
+    expect(component).toMatchSnapshot();
+  });
+});
